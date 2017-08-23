5f4965a6558ed211710379b51c5e5edd80dd3a72
snona <nona.tanuki@gmail.com>
2017-08-24 02:03:24 +0900

    containerまで一旦

--- src/containers/Chat.js
+++ src/containers/Chat.js
@@ -41,20 +41,30 @@ class _Chat extends Component {
     ChatAction.unListenMessages(roomId);  // 発言情報の自動取得終了
   }
 
+  _sendMessage = (message) => {
+    const { roomId } = this.props;
+    ChatAction.sendMessage(roomId, message);
+  };
+
+  _getDiceSystem = (system) => {
+    DiceBotAction.getSystem(system);
+  };
+
   render() {
+    const { system, systems, messages, characters }
     const { layout } = this.props;
     return (
       <div style={{ margin: 10, height: '100%' }} >
         <ChatLog
-          messages={this.state.messages}
+          messages={messages}
           layout={layout}
         />
         <ChatInput
-          sendMessage={message => ChatAction.sendMessage(this.props.roomId, message)}
-          systems={this.state.systems}
-          system={this.state.system}
-          selectSystem={(system) => DiceBotAction.getSystem(system)}
-          characters={this.state.characters}
+          sendMessage={this._sendMessage}
+          systems={systems}
+          system={system}
+          selectSystem={this._getDiceSystem}
+          characters={characters}
         />
       </div>
     );
